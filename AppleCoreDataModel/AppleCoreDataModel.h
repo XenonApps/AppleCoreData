0fe33593cdf62cb0d07edbbcbc7c6bfee158c3e2
Iain Caldwell <xenonbusinessapps@gmail.com>
2022-01-02 13:01:20 +0000

    Initial Commit

---
+++ AppleCoreDataModel/AppleCoreDataModel.h
@@ -0,0 +1,18 @@
+//
+//  AppleCoreDataModel.h
+//  AppleCoreDataModel
+//
+//  Created by Iain Caldwell on 02/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppleCoreDataModel.
+FOUNDATION_EXPORT double AppleCoreDataModelVersionNumber;
+
+//! Project version string for AppleCoreDataModel.
+FOUNDATION_EXPORT const unsigned char AppleCoreDataModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleCoreDataModel/PublicHeader.h>
+
+
